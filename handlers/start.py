5e9aa406136bad2a9ee3a08d5023ff2694e34969
Salikhemx <salikhemx@gmail.com>
2026-08-06 15:44:15 -0700

    New Start Menu

--- handlers/start.py
+++ handlers/start.py
@@ -1,11 +1,44 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="🎬 Kanal",
+                    url="https://t.me/kinolar12026"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="👨‍💻 Admin",
+                    url="https://t.me/Kino_Go2026_bot"
+                )
+            ]
+        ]
+    )
+
     await message.answer(
-        "🎬 Kino botga xush kelibsiz!\n\n📥 Kino kodini yuboring."
+        """
+🎬 <b>Kino Go botiga xush kelibsiz!</b>
+
+🎥 Eng yangi HD kinolar
+🍿 Seriallar
+🎞 Multfilmlar
+
+📥 Kino kodini yuboring va kinoni darhol oling.
+
+⭐ Sizga maroqli tomosha tilaymiz!
+        """,
+        reply_markup=keyboard
     )
